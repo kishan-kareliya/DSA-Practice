ef68d354f5c2bb7fd8db3d4e1059f2aa5efb44d8
kishan-kareliya <kareliyakishan007@gmail.com>
2024-08-20 13:09:28 +0530

    solve fibonacci questions

---
+++ src/Functions/FibonacciSeries.java
@@ -0,0 +1,27 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class FibonacciSeries {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = s.nextInt();
+        fibonacciSeries(num);
+    }
+    public static void fibonacciSeries(int num) {
+        if(num <= 0) {
+            System.out.println("Invalid Fibonacci Number");
+            return;
+        }
+        int a = 0;
+        int b = 1;
+        System.out.print(a + "," + b);
+        for(int i =2;i<num;i++){
+            int temp = a;
+            a = b;
+            b = temp + a;
+            System.out.print("," + b);
+        }
+    }
+}
---
+++ src/Functions/NthFibonacciNumber.java
@@ -0,0 +1,28 @@
+package Functions;
+
+import java.util.Scanner;
+
+public class NthFibonacciNumber {
+    public static void main(String[] args) {
+        Scanner s = new Scanner(System.in);
+        System.out.print("Enter Number: ");
+        int num = s.nextInt();
+        int ans = fibonacciNumber(num);
+        System.out.println(ans);
+    }
+    public static int fibonacciNumber(int num){
+        int a = 0;
+        int b = 1;
+        if(num == 0){
+            return a;
+        }
+        else{
+            for(int i=1;i<num;i++){
+                int temp = a;
+                a = b;
+                b = temp + a;
+            }
+            return b;
+        }
+    }
+}
