6ba66b950ddc2c43fe37d695f5c84d52b64ce53f
kishan-kareliya <kareliyakishan007@gmail.com>
2025-02-05 22:24:36 +0530

    solve find a floor and celing of a number using binary search

--- src/SearchingAndSorting/FloorOfNumber.java
+++ src/SearchingAndSorting/FloorOfNumber.java
@@ -1,27 +1,29 @@
 package SearchingAndSorting;
 
-public class CelingOfNumber {
+public class FloorOfNumber {
     public static void main(String args[]) {
         int[] arr = {12, 13, 15, 16,18, 20};
 
-        System.out.println(findCelingOfNumber(arr, 19));
+        System.out.println(findFloorOfNumber(arr, 14));
     }
-    public static int findCelingOfNumber(int[] arr,int target){
+    public static int findFloorOfNumber(int[] arr,int target){
         int left = 0;
         int right = arr.length - 1;
 
-        while(left<=right){
-            int mid = (left + right)/2;
+        while(left <= right){
+
+            int mid = (left+right)/2;
+
             if(arr[mid] == target){
                 return arr[mid];
             }
-            else if(arr[mid]>target){
+            else if(arr[mid] > target){
                 right = mid - 1;
             }
             else{
                 left = mid + 1;
             }
         }
-        return arr[left];
+        return arr[right];
     }
 }
